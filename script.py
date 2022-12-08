1414ad47f46f928cd28d9b91bc2f4e049cd046a8
christoffer-s <73720679+christoffer-s@users.noreply.github.com>
2022-12-08 19:38:42 +0000

    test

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
-txt = input("txt: ")
+def f(x):
+    return (x + 1) ** 2
 
-print(txt)
+
+a = float(input("Skriv a verdien: "))
+nøyaktighet = 0.00000001
+pres = 6
+
+print(f"Øvre grenseverdi: f({round(a - nøyaktighet, pres)}) = {round(f(a - nøyaktighet), pres)}")
+print(f"Nedre grenseverdi: f({round(a + nøyaktighet, pres)}) = {round(f(a + nøyaktighet), pres)}")
