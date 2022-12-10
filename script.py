d0f66b008fded1a5f7b5b6c5ec9d86311a6ea834
christoffer-s <73720679+christoffer-s@users.noreply.github.com>
2022-12-10 21:58:32 +0000

    sadas

--- script.py
+++ script.py
@@ -1,10 +1,1 @@
-def f(x):
-    return (x + 1) ** 2
-
-
-a = float(input("Skriv a verdien: "))
-nøyaktighet = 0.00000001
-pres = 6
-
-print(f"Øvre grenseverdi: f({round(a - nøyaktighet, pres)}) = {round(f(a - nøyaktighet), pres)}")
-print(f"Nedre grenseverdi: f({round(a + nøyaktighet, pres)}) = {round(f(a + nøyaktighet), pres)}")
+morra di er mann
