b10dfcbb6f8e46916c0cfedbdc9e383bb6eee3b8
christoffer-s <73720679+christoffer-s@users.noreply.github.com>
2023-01-19 08:06:57 +0000

    oui oui

--- debuger.py
+++ debuger.py
@@ -1,30 +1,18 @@
-def integer(n):
-    if isinstance(n, list):
-        for i, num in enumerate(n):
-            try:
-                if float(num).is_integer():
-                    n[i] = int(float(num))
-                
-                else:
-                    n[i] = float(num)
-            
-            except:
-                continue
-        
-        return n
+from matplotlib import pyplot as plt
+import pandas as pd
 
-    else:
-        try:
-            if float(n).is_integer():
-                return int(float(n))
-            
-            else:
-                return float(n)
-        except:
-            return n
-    
-    
 
-tall = ["hello", "4.2", 4.2, 4, 2, 54.0, "54.0"]
-    
-print(integer(tall))
+data = pd.read_csv("ssbmedier.csv", index_col = 0, skiprows = (0, 1), sep =";", na_values=[".", ".."], encoding = "latin-1")
+
+K = []   
+startVerdi = 2010   
+for i in range(0, 10):
+    K.append(startVerdi + i)
+data.columns = K  
+
+plt.plot(data)
+plt.show()
+
+
+
+
