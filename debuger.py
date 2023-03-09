d28fcae3e3a057a811730bacb7d02b4acfb8e5d5
christoffer-s <73720679+christoffer-s@users.noreply.github.com>
2023-03-09 20:38:37 +0000

    dsd

--- debuger.py
+++ debuger.py
@@ -1,4 +1,24 @@
-resistors = [10, [2, [4, 2], [2, 6]], [5]]
-def check_relation(r):
-    for index, val in enumerate(r):
+resistors = [[10], [5]]
+
+def resistance(r, index=0):
+    typ = isinstance(r, list)
+    parallel_i = []
+    if typ:
+        for i, val in enumerate(r):
+            if isinstance(val, list):
+                if len(val) > 1:
+                    resistance(val, i)
+                
+                else:
+                    
+                     
+            
+
+
+    
+    else:
+        return sum(r)
+
+print(resistance(resistors))
         
+    
