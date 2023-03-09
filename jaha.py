3458b2c75223c2d51f1827e47cdfc92a2134df16
christoffer-s <73720679+christoffer-s@users.noreply.github.com>
2023-03-09 21:24:01 +0000

    dsd

--- jaha.py
+++ jaha.py
@@ -1,4 +1,34 @@
-def f(x):
-    return x
+'''Eksempel program for dokumentasjon av kode, her skal programets virkemåte beskrives'''
 
-print(f(4))
+
+def func_name(var1: str, var2: int) -> str:
+    '''
+    Her skal funksjonenes I/O og virkemåte beskrives i korte trekk\n
+    Input:  var1: string
+            var2: integer\n 
+    Output: var1: string
+            var2: string
+    '''
+    
+    # merking av relevant info for utvilke:
+    # bugs / improvments / To-Do / notes
+    var1 = str(var1)
+    var2 = str(var2)
+
+    return var1, var2
+
+print(func_name.__annotations__)
+print(func_name.__doc__)
+
+
+def avg(num: list) -> float:
+    '''
+    Regner ut gjennomsnitt av en liste, 
+    kan kun inneholde float og int elementer\n
+    Input:  num: list
+    Output: avg: float 
+    '''
+    return sum(num) / len(num)
+
+
+print(avg([0, 100]))
