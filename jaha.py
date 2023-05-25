c5781bb41d4d454929cf08ad768821de7d16913b
christoffer-s <73720679+christoffer-s@users.noreply.github.com>
2023-05-25 08:18:11 +0200

    sdsd

--- jaha.py
+++ jaha.py
@@ -1,34 +1,7 @@
-'''Eksempel program for dokumentasjon av kode, her skal programets virkemåte beskrives'''
+from matplotlib import pyplot as plt
+import pandas as pd
 
 
-def func_name(var1: str, var2: int) -> str:
-    '''
-    Her skal funksjonenes I/O og virkemåte beskrives i korte trekk\n
-    Input:  var1: string
-            var2: integer\n 
-    Output: var1: string
-            var2: string
-    '''
-    
-    # merking av relevant info for utvilke:
-    # bugs / improvments / To-Do / notes
-    var1 = str(var1)
-    var2 = str(var2)
+data = pd.read_csv("co2.csv")
 
-    return var1, var2
-
-print(func_name.__annotations__)
-print(func_name.__doc__)
-
-
-def avg(num: list) -> float:
-    '''
-    Regner ut gjennomsnitt av en liste, 
-    kan kun inneholde float og int elementer\n
-    Input:  num: list
-    Output: avg: float 
-    '''
-    return sum(num) / len(num)
-
-
-print(avg([0, 100]))
+plt.plot(data)
