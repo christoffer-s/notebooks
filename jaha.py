37f77b8c1c3a2767a93d3a890e85151952d02f80
christoffer-s <73720679+christoffer-s@users.noreply.github.com>
2024-06-04 20:01:33 +0200

    sui

--- jaha.py
+++
@@ -1,6 +0,0 @@
-def resistance(r: list):
-    return r
-
-x = [1, 2, 3]
-
-print(resistance(x))
---
+++ main.py
@@ -0,0 +1,14 @@
+def code(n: int) -> None:
+    for i in range(n):
+        print("Hello Pavel")
+
+
+
+def main() -> None:
+    n = 2.3
+    code(n)
+    print('finised')
+
+
+if __name__ == '__main__':
+    main()
